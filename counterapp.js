92f996f25531783f22d88e923ef1e1c754c02f3e
Johnson Kanyi <125729582+kanyijohn@users.noreply.github.com>
2024-02-08 15:07:49 +0300

    counterapp.js

---
+++ counterapp.js
@@ -0,0 +1,24 @@
+function App () {
+    const [count, setCount] = React.useState (0)
+
+      function add() {
+        setCount(prevCount=> prevCount + 1)
+      }
+
+      function subtract () {
+        setCount (prevCount=> prevCount - 1)
+
+      }
+
+       return (
+          <div className="counter" >
+            <button className="counter--minus" onClick={subtract}>-</button>
+            <div className="counter--count">
+                <h1>{count}</h1>
+            </div>
+            <button className="counter--plus" onClick={add}>+</button>
+          </div>
+      )
+}
+
+ReactDOM.render (<App />, document.getElementById("root"))
